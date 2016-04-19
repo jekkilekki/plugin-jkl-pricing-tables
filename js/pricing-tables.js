d94d1f8523c95daf0cc40eb7365295705cf55541
Aaron Snowberger <jekkilekki@gmail.com>
2016-04-19 15:26:37 +0900

    Add $, /, * catching functions in JS + restyle slightly

--- js/pricing-tables.js
+++ js/pricing-tables.js
@@ -1,4 +1,27 @@
-jQuery(document).ready(function($) {
+/**
+ * Main jQuery function that controls all the adding of <span> tags, classes, 
+ * and resizing/responsiveness for the pricing tables
+ * 
+ * @since   0.0.1
+ * @param   jQuery  $
+ */
+
+/**
+ * Function that takes care of adding additional span classes to each individual box
+ * 1. Removes * and adds "Recommended" to the default boxes
+ * 2. Surrounds any dollar signs ($) in the price field with <span> tags for better styling
+ * 3. Surrounds any "per" units (/month) in the price field with <span> tags for better styling
+ */
+
+jQuery( document ).ready( function( $ ) {
+    
+    /*
+     * Colors array
+     */
+    var colors = [
+        
+    ];
+    
     jQuery('.pricing-tables li:not(li li)').addClass('pricing-table');
     jQuery('.pricing-tables ol ol li:nth-child(1)').addClass('price');
     jQuery('.pricing-tables ol ol li:nth-child(2)').addClass('description');
@@ -11,6 +34,8 @@ jQuery(document).ready(function($) {
         return( jQuery(this).css("display") != "none" );
     }).length;
     
+    // Holds the position of the recommended table (defaults to 0 if not set)
+    var recommended_position = 0;
     
     function resizeTables() {
         // Calculate the width of each Pricing Table
@@ -19,35 +44,96 @@ jQuery(document).ready(function($) {
         
         if ( screenWidth > 600 ) { // dunno why I have to -2 each time...
             jQuery('.pricing-table').width( width + "%" );
-            jQuery('.pricing-table').css('margin-left', 'auto');
+            jQuery('.pricing-table').css('margin-left', '0%');
+            jQuery('.pricing-table').css('margin-right', '0%');
         } else if ( screenWidth > 400 ) {
-            jQuery('.pricing-table').width( 31 + "%" );
-            jQuery('.pricing-table').css('margin-left', 'auto');
+            jQuery('.pricing-table').width( 33 + "%" );
+            jQuery('.pricing-table').css('margin-left', '0%');
+            jQuery('.pricing-table').css('margin-right', '0%');
         } else {
-            jQuery('.pricing-table').width( 68 + "%" );
+            jQuery('.pricing-table').width( 70 + "%" );
             jQuery('.pricing-table').css('margin-left', '15%');
+            jQuery('.pricing-table').css('margin-right', '15%');
         }
     }
     
     jQuery(window).resize(function() { resizeTables(); });
     resizeTables();
+    handle_price();
     
     // Make SECOND table pop-out
-    jQuery('.pricing-table:nth-child(2)').addClass('pop-out');
+    // jQuery('.pricing-table:nth-child(2)').addClass('pop-out');
+    
+    /*
+     * This code determines which table to "pop" as the recommended table
+     */
+    // get all the tables
+    var tables = jQuery( '.pricing-table' );
+    // Search every table
+    var position_count = 0;
+    for ( var i = 0; i < tables.length; i++ ) {
+        // if you find a * - this means it's recommended
+        
+        if( tables[i].innerHTML.indexOf( '*' ) !== -1 ) {
+            
+            // FIRST, replace the star with a span class
+            var to_replace = tables[i].innerHTML.substring( tables[i].innerHTML.indexOf('*'), tables[i].innerHTML.indexOf('*')+1 );
+            var with_this = '<span class="recommended">Recommended</span>';
+            tables[i].innerHTML = tables[i].innerHTML.replace( to_replace, with_this );
+            
+            if( position_count == 0  ) {
+                // SECOND, determine the proper position
+                // only use the FIRST possible_position to set as a "pop-out"
+                var pos = i+1;
+                recommended_position = '.pricing-table:nth-child(' + pos + ')';
+
+                // Here we should try to remove the * or add a <span> class with extra details
+                //var str = tables[--i].innerHTML;
+                //alert( str );
+                //str = str.replace( '*', '<span class="recommended">Recommended</span>' );
+                //alert( str );
+                //jQuery( recommended_position ).html(str);
+                jQuery( recommended_position ).addClass( 'pop-out' );
+                ++position_count;
+            }
+            
+        }
+        
+    }
 
     // Make a table I hover over pop-out (then the default #2 remain popped after mouseoff)
     jQuery('.pricing-table').hover(
             function() {
-                if( jQuery('.pricing-table:nth-child(2)').hasClass('pop-out') ) {
-                    jQuery('.pricing-table:nth-child(2)').removeClass('pop-out');
+                if( jQuery( recommended_position ).hasClass('pop-out') ) {
+                    jQuery( recommended_position ).removeClass('pop-out');
                 }
                 jQuery(this).addClass('pop-out');
             }, function() {
                 jQuery(this).removeClass('pop-out');
-                if( jQuery('.pricing-table:nth-child(2)').hasClass('pop-out') ) { /* do nothing */ }
+                if( jQuery( recommended_position ).hasClass('pop-out') ) { /* do nothing */ }
                 else { 
-                    jQuery('.pricing-table:nth-child(2)').addClass('pop-out'); 
+                    jQuery( recommended_position ).addClass('pop-out'); 
             }
     });
+    
+    /*
+     * Function to add better styles to the pricing field
+     */
+    function handle_price() {
+        var price_field = jQuery( '.price' );
+        for( var i = 0; i < price_field.length; i++ ) {
+            // Add styles to the / component
+            // This MUST come first or it will detect the / in </span> for the dollar sign as well
+            if( price_field[i].innerHTML.indexOf( '/' ) !== -1 ) {
+                var to_replace = price_field[i].innerHTML.substring( price_field[i].innerHTML.indexOf('/') );
+                var with_this = '<span class="installment">' + to_replace + '</span>';
+                price_field[i].innerHTML = price_field[i].innerHTML.replace( to_replace, with_this );
+            }
+            // Add styles to the dollar sign
+            if( price_field[i].innerHTML.indexOf( '$' ) !== -1 ) {
+                price_field[i].innerHTML = price_field[i].innerHTML.replace( '$', '<span class="dollar-sign">$</span>' );
+            }
+        }
+    }
 });
 
